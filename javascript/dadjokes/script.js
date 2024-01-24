ad3d51cd4b19921fbe5894a5b97fd7c8ebfb6a3d
7ommyShelby <aryaalsaher@gmail.com>
2024-01-24 17:35:57 +0530

    img search

--- javascript/dadjokes/script.js
+++ javascript/dadjokes/script.js
@@ -1,19 +1,25 @@
 
 let dj = document.querySelector('p');
 
-function jokes() {
-    fetch('https://api.api-ninjas.com/v1/dadjokes?limit=1', {
+async function jokes() {
+  let res =  await fetch('https://api.api-ninjas.com/v1/dadjokes?limit=1', {
         headers: {
             "X-Api-Key": "4MKMfzIIx+9KlgmjzgKioQ==ioLnKQ9BluRp6LYV"
         }
-    })
-        .then((result) => {
-           return result.json()
-        }).then((e) => {
-              console.log(e[0].joke);
-            return dj.innerText = e[0].joke
+    }) 
 
-        })
+    let dat = await res.json();
+    console.log(dat)
+    dj.innerText = dat[0].joke
+
+    
+        // .then((result) => {
+        //     return result.json()
+        // }).then((e) => {
+        //     console.log(e[0].joke);
+        //     return dj.innerText = e[0].joke
+        // })
+
 }
 
 document.querySelector("button").addEventListener('click', jokes)
---
+++ javascript/imgsearch/script.js
@@ -0,0 +1,42 @@
+let container = document.querySelector('#search');
+let query = document.querySelector('input');
+let display = document.querySelector('.show');
+let more = document.querySelector('.data');
+
+
+let x = 1;
+
+async function photo() {
+    let data = await fetch(`https://api.unsplash.com/search/photos?page=${x}&query=${query.value}&client_id=BcEUItfGZTTH9aVHXJMn9pR1OtM1Scbf1McGJRq8Is8`)
+
+    let ans = await data.json();
+    console.log(ans.results);
+   
+    ans.results.forEach(element => {
+        let div = document.createElement('div');
+        let tags = document.createElement('img');
+        let txt = document.createElement('span');
+        tags.setAttribute('src', element.urls.regular)
+        txt.innerText = (element.alt_description)
+        // console.log(element.description)
+        display.appendChild(div)
+        div.append(tags, txt)
+    });
+
+}
+
+container.addEventListener('click', (e) => {
+    display.innerHTML = "";
+    x = 1;
+    photo()
+    more.style.display = 'block'
+    console.log("once",x);
+})
+
+
+more.addEventListener('click', (e) => {
+    x++;
+    photo()
+     console.log("final",x);
+})
+
