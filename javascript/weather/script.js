6f236438db586657df379b3ffe0ca34bdcd7a922
7ommyShelby <aryaalsaher@gmail.com>
2024-02-07 17:45:54 +0530

    weather

--- javascript/weather/script.js
+++ javascript/weather/script.js
@@ -2,55 +2,91 @@ let input = document.querySelector('input');
 let display = document.querySelector('.display');
 
 
+switch (new Date().getDay()) {
+    case 0:
+        day = "Sunday";
+        break;
+    case 1:
+        day = "Monday";
+        break;
+    case 2:
+        day = "Tuesday";
+        break;
+    case 3:
+        day = "Wednesday";
+        break;
+    case 4:
+        day = "Thursday";
+        break;
+    case 5:
+        day = "Friday";
+        break;
+    case 6:
+        day = "Saturday";
+}
+console.log(day);
 async function weather() {
-    // let x = await fetch(`https://api.tomorrow.io/v4/weather/realtime?location=${input.value}&apikey=3NkvA9A6qRo3PHIpnfjOR9ZWTDOAxgjF`, {
-    //     header: {
-    //         'accept': 'application/json'
-    //     }
-    // })
 
+    try {
+        let x = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=cc436684a80bb4774325274fab92c0da&units=metric `)
 
-    // let y = await x.json();
-    console.log(input.value);
+        input.value ="";
+        
+        let y = await x.json();
+        console.log(y);
 
-    display.innerHTML = `
-                <p>Temp <span>${y.data.values.temperature}</span></p>
-                <p>pressure surface <span>${y.data.values.pressureSurfaceLevel}</span></p>
-                <p>visiblity <span>${y.data.values.visibility}</span></p>
-                <p>humidity <span>${y.data.values.humidity}</span></p>
-    `
+        let src
 
+        if (y.weather[0].main == 'Clouds') {
+            src = './clouds.png'
+        } else if (y.weather[0].main == 'Rain') {
+            src = './storm.png'
+        } else if (y.weather[0].main == 'Clear') {
+            src = './sun.png'
+        } else if (y.weather[0].main == 'Drizzle') {
+            src = './drizzle.png'
+        } else if (y.weather[0].main == 'Mist') {
+            src = './haze.png'
+        } else if (y.weather[0].main == 'Haze') {
+            src = './haze1.png'
+        } else if (y.weather[0].main == 'Smoke') {
+            src = './carbon-dioxide.png'
+        }
+
+        display.innerHTML = `
+        <div class="left">
+            <p>${y.name}</p>
+            <p>${day}</p>
+            <p>Pressure : ${y.main.pressure} hPa</p>
+            <p class = "humid"><span class="material-symbols-outlined">
+            humidity_low
+            </span>: ${y.main.humidity}%</p>
+            <p class="wind">
+            <span class="material-symbols-outlined">
+            air
+            </span>: ${y.wind.speed} km/hr
+            </p>
+        </div>
+
+        <div class="mid">
+            <img class="weather" src=${src} alt="">
+            <p>${y.weather[0].description}</p>
+        </div>
+    
+            <div class="right">
+                <p class="temp">${y.main.temp}°C</p>
+            </div>
+        `
+
+    }
+
+    catch (error) {
+        console.log(error);
+        display.innerHTML = `City Not Found`
+    }
 
 }
 
-
-function throttle(mainFunction, delay) {
-    let timerFlag = null; 
-
-    return (...args) => {
-      if (timerFlag === null) { 
-        mainFunction(...args); 
-        timerFlag = setTimeout(() => { 
-          timerFlag = null; 
-        }, delay);
-      }
-    };
-  }
-  
-
-function debounce(func, timeout = 1500) {
-    let timer = 0;
-    return (...args) => {
-        const now = new Date().getTime();
-        if ((now - timer) < timeout)
-            return;
-        timer = now;
-        func(...args)
-    };
-}
-
-const processChange = debounce(weather);
-
-window.addEventListener('keyup', processChange)
+document.querySelector('button').addEventListener('click', weather)
 
 
